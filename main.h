2b693a4274050280456d6e0bc2621c11e1de7eef
Anthony-ndegwa <anthony.n.ndegwa@gmail.com>
2022-10-19 08:56:23 +0300

    Update main.h

--- main.h
+++ main.h
@@ -15,8 +15,8 @@ typedef struct print
         int (*f)(va_list);
 } print_t;
 
-int _printf(const char *format, ...);
 int _putchar(char c);
+int _printf(const char *format, ...);
 int print_c(va_list c);
 int print_s(va_list s);
 int print_i(va_list i);
