7b219f2972f843c544b4787a9f98664e48c2f028
DicksonWachai <wachaidickson@gmail.com>
2023-11-14 23:52:09 -0800

    function that produces output according to a format

--- _printf.c
+++ _printf.c
@@ -1,82 +1,58 @@
-#include <stdlib.h>
+#include <stdio.h>
 #include <stdarg.h>
 #include "main.h"
 /**
- * _printf - products output according to format
- * @format: returns the format
+ * _printf - prints output format
+ * @format: the format to be printed
  *
- *
- * Return: The output of the format
+ * Return: Number of characters printed
  */
-int print_c(char c, ...)
-{
-	va_list args;
-
-	va_start(args, c);
-
-	char ch = va_arg(args, int);
-
-	_putchar(ch);
-
-	va_end(args);
-
-	return (1)
-}
-int print_s(char *s, ...)
+int _printf(const char *format, ...)
 {
 	int count = 0;
+	const char *str;
 
 	va_list args;
 
-	va_start(args, *s);
-	const char *str = s;
-	if (str == NULL)
+	va_start(args, format);
+
+	while (*format != '\0')
 	{
-		str = "(null)";
-	}
-	while (str[count] != '\0')
-	{
-		_putchar(str[count]);
-		count++;
+		if (*format == '%')
+		{
+			format++;
+			switch (*format)
+			{
+				case 'c':
+					_putchar(va_arg(args, int));
+					count++;
+					break;
+				case 's':
+					str = va_arg(args, char*);
+					while (*str != '\0')
+					{
+						_putchar(*str);
+						str++;
+						count++;
+					}
+					break;
+				case '%':
+					_putchar('%');
+					count++;
+					break;
+				default:
+					_putchar('%');
+					_putchar(*format);
+					count += 2;
+					break;
+			}
+		}
+		else {
+			_putchar(*format);
+			count++;
+		}
+		format++;
 	}
 	va_end(args);
 	return (count);
-}
-int _printf(const char *format, ...) {
-    va_list args;
-    va_start(args, format);
-
-    int printed_chars = 0;
-
-    while (*format != '\0') {
-        if (*format == '%') {
-            format++; // Move to the next character after %
-
-            // Handle the conversion specifiers
-            switch (*format) {
-                case 'c':
-                    printed_chars += print_c(va_arg(args, int));
-                    break;
-                case 's':
-                    printed_chars += print_s(va_arg(args, const char *));
-                    break;
-                case '%':
-                    _putchar('%');
-                    printed_chars++;
-                    break;
-                default:
-                    _putchar('%'); // Print the % character if not a valid specifier
-                    _putchar(*format);
-                    printed_chars += 2;
-                    break;
-            }
-        } else {
-            _putchar(*format);
-            printed_chars++;
-        }
-        format++; // Move to the next character in the format string
-    }
-
-    va_end(args);
-    return printed_chars;
 }
